2d08179e328c2b65557a58fb640eb4bc23d3942a
micd84 <michal.s.debski@gmail.com>
2018-04-14 15:18:40 +0200

    added domain project

---
+++ TaskList.Domain/Entities/Task.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskList.Domain.Entities
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public string Title { get; set; }
+    }
+}
